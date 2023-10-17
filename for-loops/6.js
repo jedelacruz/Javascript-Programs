67dd53eb7264b09e7e0101d8f4f4843eaf6b8ae6
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-10-17 09:06:43 +0800

    Create 6.js

---
+++ for-loops/6.js
@@ -0,0 +1,8 @@
+// Multiplication Table: Create a program that generates the multiplication table for a given number N using a for loop, printing the results for 1 to 10.
+
+let n = 1;
+
+for (let i = 1; i <= 10; i++) {
+    let result = n * i;
+    console.log(`${n} x ${i} = ${result}`);
+}
