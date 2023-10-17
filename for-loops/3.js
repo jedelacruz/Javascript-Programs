9f2caac959b19cb8fc6707524d2f5ec5ebac71cf
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-10-17 08:59:05 +0800

    Create 3.js

---
+++ for-loops/3.js
@@ -0,0 +1,10 @@
+// Sum of Integers: Write a program that calculates the sum of all integers from 1 to N using a for loop.
+
+n = 10
+sumOfAll = 0
+
+for(i = 1; i <= n; i++){
+    sumOfAll += i
+}
+
+console.log("The sum of all is", sumOfAll)
