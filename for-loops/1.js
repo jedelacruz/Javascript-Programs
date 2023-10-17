839c7491126d55d143582910bd39d94b20f22c7c
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-10-17 08:51:26 +0800

    Create 1.js

---
+++ for-loops/1.js
@@ -0,0 +1,8 @@
+// Counting from 1 to N: Write a program that uses a for loop to print the numbers from 1 to N, where N is a given positive integer.
+
+n = 25
+
+for(let i = 1; i <= n; i++){
+    console.log(i)
+}
+
