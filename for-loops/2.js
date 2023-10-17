b5a738ecc9bfbe034f7c685b52f9d8f2901bbd90
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-10-17 08:53:24 +0800

    Create 2.js

---
+++ for-loops/2.js
@@ -0,0 +1,8 @@
+// Counting Backwards: Create a program that counts backward from a given positive integer N to 1 using a for loop.
+
+n = 0
+
+for(let i = 25; i >= n; i--){
+    console.log(i)
+}
+
