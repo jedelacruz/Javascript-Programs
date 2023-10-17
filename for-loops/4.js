82630d3ea4722273433ef54bae9ed1450f616cb1
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-10-17 09:00:21 +0800

    Create 4.js

---
+++ for-loops/4.js
@@ -0,0 +1,9 @@
+// Even Numbers: Write a program to print all even numbers from 1 to N using a for loop.
+
+let n = 12
+
+for(let i = 0; i <= n; i++){
+    if (i % 2 == 0){
+        console.log(i)
+    }
+}
